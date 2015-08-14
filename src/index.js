8fd02a8fa7ce5211efd1e83b1d22f1710a99e8df
Rob Eisenberg <rob@bluespire.com>
2015-08-14 01:41:32 -0400

    feat(all): add more type info

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import {Animator} from 'aurelia-templating';
 import {CssAnimator} from './animator';
 
-export function configure(config, cb){
+export function configure(config: Object, callback?:(animator:CssAnimator) => void){
   var animator = config.container.get(CssAnimator);
+
   Animator.configureDefault(config.container, animator);
-  if(typeof cb === 'function') {
-    cb(animator);
+
+  if(typeof callback === 'function') {
+    callback(animator);
   }
 }
